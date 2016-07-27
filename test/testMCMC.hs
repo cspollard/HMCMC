4159491404bb836ae7965e4c9a1a9da31731cab3
Chris Pollard <cspollard@gmail.com>
2016-07-27 13:38:12 +0100

    working on basic example.

--- test/testMCMC.hs
+++ test/testMCMC.hs
@@ -1,13 +1,46 @@
+{-# LANGUAGE RankNTypes #-}
+
 module Main where
 
-import Numeric.MCMC.Metropolis
-import Statistics.Distribution
+import System.Random.MWC.Probability (Gen, withSystemRandom, asGenIO)
+import qualified System.Random.MWC.Probability as MWC
+import Control.Monad.Trans.State.Strict (execStateT)
+
+
 import Statistics.Distribution.Poisson
+import Statistics.Quantile
+
+import Data.Sampling.Types (Transition(..), Target(..))
+
 import Data.HEPModel
 
+import Conduit
+
+import Numeric.MCMC
+
+-- ~from https://hackage.haskell.org/package/declarative-0.2.1/docs/src/Numeric-MCMC.html#mcmc
+
+-- A Markov chain driven by an arbitrary transition operator.
+chain
+  :: PrimMonad m
+  => Transition m b
+  -> b
+  -> Gen (PrimState m)
+  -> Producer m b
+chain transition = loop where
+  loop state prng = do
+    next <- lift (MWC.sample (execStateT transition state) prng)
+    yield next
+    loop next prng
+
+
 main :: IO ()
-main = withSystemRandom . asGenIO $ mcmc 99999 0.1 [1, 1, 1, 1] testLH
-    where testData = [1, 2, 3, 4]
+main = withSystemRandom . asGenIO $
+        \g -> chain (slice 1) c g =$ takeC 99999 $$ printC
+
+    where c = Chain t (testLH [1, 1, 1, 1]) [1, 1, 1, 1] Nothing
+          t = Target testLH Nothing
+          testData = [1, 50, 25, 100]
           testLH xs = if any (<= 0) xs
-                         then (-1.0/0.0)
+                         then (-1e100)
                          else modelLLH testData (map poisson xs)
