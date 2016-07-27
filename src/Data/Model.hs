26009bcf86407e3e9788869d44e30d235a4ccc9d
Chris Pollard <cspollard@gmail.com>
2016-07-27 19:17:01 +0100

    reverting to lists for now.

--- src/Data/Model.hs
+++ src/Data/Model.hs
@@ -5,46 +5,27 @@
 
 module Data.Model where
 
+import Data.Map (Map)
+import qualified Data.Map as M
+
 import Statistics.Distribution
 import Statistics.Distribution.Poisson
 import Control.Monad.Primitive (PrimMonad)
 
-import Data.FixedList
-import Data.Foldable
+type SFHist = [Double]
+type PredHist = [PoissonDistribution]
+type DataHist = [Int]
 
-import Control.Lens.Type
-import Control.Lens.At
-import Control.Lens.Indexed
-
-type PredHist f = Cons f PoissonDistribution
-type DataHist f = Cons f Int
-
-type instance Index (Cons f a) = Int
-type instance Index (Nil a) = Int
-type instance IxValue (Cons f a) = a 
-type instance IxValue (Nil a) = a 
-
-instance (Ixed (f a), Index (f a) ~ Int, IxValue (f a) ~ a, FixedList f) => Ixed (Cons f a) where
-    ix k f xs0@(x :. xs) | k < 0     = pure xs0
-                         | k == 0    = (:. xs) <$> f x
-                         | otherwise = (x :.) <$> ix (k-1) f xs
-
-instance Ixed (Nil a) where
-    ix _ _ _ = pure Nil
-
-
-instance (FixedList f, FoldableWithIndex Int f) => FoldableWithIndex Int (Cons f) where
-    ifoldMap g (x :. xs) = g 0 x `mappend` ifoldMap (g . (+1)) xs
-
-instance FoldableWithIndex Int Nil where
-    ifoldMap _ _ = mempty
-
+zipWithLen :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWithLen f (a:as) (b:bs) = f a b : zipWithLen f as bs
+zipWithLen _ [] []         = []
+zipWithLen _ _ _           = error "zipping lists of different lengths"
 
 -- the log likelihood of a prediction histogram given a data histogram
-predHistLLH :: FixedList f => DataHist f -> PredHist f -> Double
-predHistLLH dh ph = sum $ logProbability <$> ph <*> dh
+predHistLLH :: DataHist -> PredHist -> Double
+predHistLLH dh ph = sum $ zipWithLen logProbability ph dh
 
-{-
+
 -- a process's normalization is consistent across regions
 type Process = Map String PredHist
 
@@ -54,20 +35,23 @@ type Prediction = Map String Process
 -- data in several regions
 type Dataset = Map String DataHist
 
+liftP :: (Double -> Double) -> PoissonDistribution -> PoissonDistribution
+liftP f = poisson . f . poissonLambda
 
-
+liftP2 :: (Double -> Double -> Double) -> PoissonDistribution -> PoissonDistribution -> PoissonDistribution
+liftP2 f p p' = poisson $ f (poissonLambda p) (poissonLambda p')
 
 scaleH :: Double -> PredHist -> PredHist
-scaleH n = omap (*n)
+scaleH n = fmap (liftP (*n))
 
 addH :: PredHist -> PredHist -> PredHist
-addH h h' = zipWith (+) (otoList h'
+addH = zipWithLen (liftP2 (+))
 
-mulH :: PredHist -> PredHist -> PredHist
-mulH = zipWith (*)
+mulH :: SFHist -> PredHist -> PredHist
+mulH sfh = fmap poisson . zipWithLen (*) sfh . fmap poissonLambda
 
 sumH :: [PredHist] -> PredHist
-sumH = foldr1 addH
+sumH = foldl1 addH
 
 
 
@@ -90,9 +74,9 @@ procNormParam :: ContDistr d => d -> String -> ModelParam
 procNormParam prior name = ModelParam (logDensity prior) (\x -> alterProc (scaleH x) name)
 
 
-procShapeParam :: ContDistr d => PredHist -> d -> String -> ModelParam
+procShapeParam :: ContDistr d => SFHist -> d -> String -> ModelParam
 procShapeParam hshape prior name = ModelParam (logDensity prior) $
-                                    (\x -> alterProc (mulH (scaleH x hshape)) name)
+                                    (\x -> alterProc (mulH (fmap (*x) hshape)) name)
 
 
 totalPrediction :: Prediction -> Process
@@ -100,20 +84,18 @@ totalPrediction = M.foldr (M.unionWith addH) M.empty
 
 
 -- TODO!!!
-expectedData :: Prediction -> Dataset
-expectedData = fmap (fmap round) . totalPrediction
+-- expectedData :: Prediction -> Dataset
+-- expectedData = fmap (fmap round) . totalPrediction
 
 
 -- the poisson likelihood of a model given the input data
 modelPoissonLLH :: Dataset -> Prediction -> Double
-modelPoissonLLH ds m = M.foldr (+) 0 $ M.intersectionWith poissHistLLH ds (fmap (fmap poisson) $ totalPrediction m)
-
+modelPoissonLLH ds m = M.foldr (+) 0 $ M.intersectionWith predHistLLH ds (totalPrediction m)
 
 
 modelLLH :: Dataset -> Prediction -> [ModelParam] -> [Double] -> Double
 modelLLH ds hpred hparams params = priorLLH + poissLLH
     where
-        priorLLH = sum $ zipWith mpPrior hparams params
-        hpred' = foldr ($) hpred (zipWith mpAlter hparams params)
+        priorLLH = sum $ zipWithLen mpPrior hparams params
+        hpred' = foldr ($) hpred (zipWithLen mpAlter hparams params)
         poissLLH = modelPoissonLLH ds hpred'
--}
--- test/testMCMC.hs
+++ test/testMCMC.hs
@@ -6,9 +6,6 @@ import System.Random.MWC.Probability (Gen, withSystemRandom, asGenIO)
 import qualified System.Random.MWC.Probability as MWC
 import Control.Monad.Trans.State.Strict (execStateT)
 
-import Data.FixedList
-import Data.Foldable
-
 import Statistics.Distribution.Poisson
 
 import Conduit
@@ -39,8 +36,8 @@ main = withSystemRandom . asGenIO $
 
     where c = Chain t (testLH initPred) initPred Nothing
           t = Target testLH Nothing
-          testData = 1 :. 100 :. 25 :. 50 :. Nil
-          initPred = 1 :. 1 :. 1 :. 1 :. Nil
+          testData = [1, 100, 25, 50]
+          initPred = [1, 1, 1, 1]
           testLH xs = if any (<= 0) xs
                          then (-1e100)
                          else predHistLLH testData (fmap poisson xs)
