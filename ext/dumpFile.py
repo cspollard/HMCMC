d7c877b827522adf203135574ab13e9aed5cdfcf
Chris Pollard <cspollard@gmail.com>
2016-07-27 19:24:52 +0100

    updating file dumping.

--- ext/dumpFile.py
+++ ext/dumpFile.py
@@ -1,55 +1,21 @@
-from sys import argv
+from sys import argv, stdout
 myargv = argv[:]
 
 import ROOT
 import json
+print "HELLO"; stdout.flush()
 
-regions = [
-        "lvJ_0tag_0additionaltag_unblind_SR",
-        "lvJ_1tag_0additionaltag_unblind_SR",
-        "lvJ_2tag_0additionaltag_unblind_SR",
-        "lvJ_0tag_1additionaltag_unblind_SR",
-        "lvJ_1tag_1additionaltag_unblind_SR",
-        "lvJ_2tag_1additionaltag_unblind_SR"
-        ]
-
-def main(fname, norm):
+def main(fname):
     fin = ROOT.TFile.Open(fname)
 
-    dmc = {}
-    for r in regions:
-        sbkg = fin.Get(r + "/Nominal/bkg_vh_m")
-        hs = sbkg.GetHists()
-
-
-        ssig = fin.Get(r + "/Nominal/sig_vh_m")
-        for h in ssig.GetHists():
-            if "1600" in h.GetName():
-                hs.append(h)
-                break
-            else:
-                pass
-
-            continue
-
-        map(lambda h: h.Scale(norm), hs)
-        dmc[r] = map(toList, hs)
-
+    d = {}
+    for k in fin.GetListOfKeys():
+        h = fin.Get(k.GetName())
+        print h; stdout.flush()
+        d[h.GetName()] = toList(h)
         continue
 
-    ddmc = {}
-    for reg, procHists in dmc.iteritems():
-        for (proc, h) in procHists:
-            if proc not in ddmc:
-                ddmc[proc] = {reg : h}
-            else:
-                ddmc[proc][reg] = h
-
-            continue
-        continue
-
-
-    print json.dumps(ddmc)
+    print json.dumps(d)
 
     return
 
@@ -60,4 +26,4 @@ def toList(h):
                 for iBin in range(1, h.GetNbinsX()+2)])
 
 if __name__ == "__main__":
-    main(myargv[1], float(myargv[2]))
+    main(myargv[1])
